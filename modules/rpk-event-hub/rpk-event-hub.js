f3a4aee9306fd62b908b4a7e72b92843be32ae55
chuanqi sun <henrysun918@gmail.com>
2017-10-17 21:28:49 -0700

    add rpk modules

---
+++ modules/rpk-event-hub/rpk-event-hub.js
@@ -0,0 +1,41 @@
+"use strict";
+
+if (window.rpk === undefined) {
+    window.rpk = {};
+}
+
+window.rpk.EventHub = (function() {
+    function EventHub() {
+        this.channels = {};
+    }
+
+    EventHub.prototype.subscribe = function(channel, subscriber) {
+        if (this.channels[channel] === undefined) {
+            this.channels[channel] = [];
+        }
+        this.channels[channel].push(subscriber);
+    }
+
+    EventHub.prototype.unsubscribe = function(channel, subscriber) {
+        let i = this.channels[channel].indexOf(subscriber);
+        if (i > -1) {
+            this.channels[channel].splice(i, 1);
+        }
+
+        if (this.channels[channel].length === 0) {
+            delete this.channels[channel];
+        }
+    }
+
+    EventHub.prototype.publish = function(channel, event) {
+        if (this.channels[channel] === undefined) {
+            return;
+        }
+
+        for (let i = 0; i < this.channels[channel].length; i++) {
+            this.channels[channel][i](event);
+        }
+    }
+
+    return EventHub;
+})();
---
+++ modules/rpk-renderer/rpk-renderer.js
@@ -0,0 +1,33 @@
+"use strict";
+
+if (window.rpk === undefined) {
+    window.rpk = {};
+}
+
+window.rpk.Renderer = (function() {
+    function Renderer() {}
+
+    Renderer.prototype.renderTemplateToSelector = function (selector, templateHTML, dataObj) {
+        const renderedHTML = renderTemplate(templateHTML, dataObj);
+        setInnerHTMLToSelector(selector, renderedHTML);
+    }
+    
+    Renderer.prototype.renderTemplate = function (templateHTML, dataObj) {
+        const replacer = function (substring, offset) {
+            const expression = substring.slice(1, -1);
+            return eval('dataObj.' + expression);
+        };
+        const templateVarRegex = /{[^{]+}/gi;
+        return templateHTML.replace(templateVarRegex, replacer);   
+    }
+    
+    Renderer.prototype.setInnerHTMLToSelector = function (selector, innerHTML) {
+        const anchors = document.querySelectorAll(selector);
+        for (let i = 0; i < anchors.length; i++) {
+            anchors[i].innerHTML = innerHTML;
+        }
+    }
+
+    return Renderer
+})();
+
